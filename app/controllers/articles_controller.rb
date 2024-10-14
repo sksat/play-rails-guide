969a260ef6814d1111c6e8801161312e1211e450
sksat <sksat@sksat.net>
2024-10-14 19:08:49 +0900

    add visible logic to article & comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       redirect_to @article
     else
-      render :edit, status: unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -44,6 +44,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
