a7576ce7baabe433d602d5d2c2085c2d88e84320
sksat <sksat@sksat.net>
2024-10-14 18:01:44 +0900

    Resourceful Routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
   # Defines the root path route ("/")
   root "articles#index"
